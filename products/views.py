69a30aba2027fcc87d74abb2433ac5fd416c30de
Windigo <jacob@fragdev.com>
2017-01-05 18:24:46 -0800

    Changed listing logic for an empty category, removed extra type argument from the listing view

--- products/views.py
+++ products/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from .models import ProductCategory, ProductType
 
 
-def listing(category_slug, type_slug):
+def listing(category_slug):
     """
     Display a product listing based on category and subcategory
     """
 
     prod_category = get_object_or_404(ProductCategory, slug=category_slug)
-
-    #TODO: get the proper syntax for get_list_or_404
     products = Product.objects.filter(category=prod_category)
 
+    #TODO: create template
     return render('products/page-listing.html', {'products': products})
