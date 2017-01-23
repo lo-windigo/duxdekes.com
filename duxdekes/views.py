982fff922428aa43a6b91e75fe682e83b0ac194b
Windigo <jacob@fragdev.com>
2017-01-23 08:36:43 -0800

    Implemented a contact page

--- duxdekes/views.py
+++ duxdekes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from products.util import live_categories
 from .util.contact import ContactForm
+from products.util import live_categories
+from django.shortcuts import redirect, render
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def about(request):
@@ -21,7 +23,45 @@ def contact(request):
     """
     Send Jeff a message
     """
-    return render(request, 'duxdekes/page-about-artist.html')
+
+    # Handle any form submissions
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        # Send message if no problems were detected
+        if form.is_valid():
+
+            message_template= """
+            Name: {name}
+            Email: {email}
+            Message:
+            {msg}
+            """
+
+            message_body = message_template.format(
+                    name=form.cleaned_data['name'],
+                    email=form.cleaned_data['email'],
+                    msg=form.cleaned_data['message'])
+
+            send_mail(settings.CONTACT_SUBJECT,
+                    message_body,
+                    settings.CONTACT_SENDER,
+                    settings.CONTACT_RECIPIENTS)
+
+            return redirect('contact_sent')
+
+    # If no submission has occurred, start a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'duxdekes/page-contact.html', {'form': form})
+
+
+def contact_sent(request):
+    """
+    Status message: Contact was successfully sent
+    """
+    return render(request, 'duxdekes/page-contact-sent.html')
 
 
 def home(request):
