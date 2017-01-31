0963b3aa59e8dee919f15f10be4d6700147ccac2
Windigo <jacob@fragdev.com>
2017-01-31 09:08:35 -0800

    Add an image placeholder SVG, and fix the product listing block

--- products/templatetags/product_tile.py
+++ products/templatetags/product_tile.py
@@ -9,7 +9,7 @@ def product_tile(product):
     """
     Return a representation of a single product for display on the website
     """
-    if(isinstance(product_value, str)):
+    if(isinstance(product, str)):
         product = Product.objects.get(slug=product)
 
     return {"product": product}
