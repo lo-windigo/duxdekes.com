2acbf67136fb8698268a8a79b0c5eae7fca4a023
Windigo <jacob@fragdev.com>
2017-02-17 12:53:45 -0800

    Start a few utility functions (may be moved under model admin) that convert Product objects to specific models

---
+++ products/util/convert.py
@@ -0,0 +1,16 @@
+from products import models
+
+
+def convert_to_finished(modeladmin, request, queryset):
+    pass
+
+
+def convert_to_instruction(modeladmin, request, queryset):
+    pass
+
+
+def convert_to_unfinished(modeladmin, request, queryset):
+    pass
+
+
+
