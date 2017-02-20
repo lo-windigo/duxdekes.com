5c200149863f383a5ba1f12204f5216f21fa202f
Windigo <jacob@fragdev.com>
2017-02-20 13:19:40 -0800

    Changed the system requirements script to propmpt user for which DB requirements to install

--- system-requirements.sh
+++ system-requirements.sh
@@ -1,36 +1,65 @@
 #!/usr/bin/env bash
 
-# Any Debian requirements should be listed here
+if [ "$USER" != "root" ]; then
+	echo "This script installs system dependencies, and requires root"
+	exit 9
+fi
+
+
+#########################
+# Requirements 
+#########################
+
 # Debian Jessie
 read -r -d '' DEBIAN_PACKAGES <<-'PACKAGES'
 	libjpeg62-turbo-dev libopenjpeg-dev libfreetype6-dev libtiff5-dev
 	liblcms2-dev libwebp-dev tk8.6-dev python3-tk python3-dev
 PACKAGES
 
-
-# MySQL dependencies:
-# libmysqlclient-dev
-
-# Postgres dependencies:
-# libpq-dev
-
-# Python package requirements
-read -r -d '' PIP_PACKAGES <<-'PACKAGES'
-	Django
-	pillow
-	psycopg2
+# MySQL dependencies
+read -r -d '' MYSQL_PACKAGES <<-'PACKAGES'
+	libmysqlclient-dev
 PACKAGES
 
-# DB-dependent pip packages
-#	mysqlclient
+# Postgres dependencies
+read -r -d '' PGSQL_PACKAGES <<-'PACKAGES'
+	libpq-dev
+PACKAGES
 
-# Try installing the debian requirements
-if [ "$USER" == "root" ]; then
 
-	apt install $DEBIAN_PACKAGES
+#########################
+# Check for permissions
+#########################
+if [ "$USER" != "root" ]; then
+	echo "This script installs system dependencies, and requires root"
+	exit 9
 fi
 
 
-# Use pip to install any other requirements
-pip install $PIP_PACKAGES
+#########################
+# Compile reqs
+#########################
 
+PACKAGES="$DEBIAN_PACKAGES"
+
+echo "Which database requirements would you like to install?"
+echo "	mysql"
+echo "	postgres"
+echo "	none (default)"
+echo -e "> "
+
+select DB in "mysql" "postgres" "none"; do
+	case $DB in
+		mysql ) PACKAGES+=MYSQL_PACKAGES; break;;
+		postgres ) PACKAGES+=PGSQL_PACKAGES; break;;
+		none|* ) break;;
+	esac
+done
+
+echo "Installing requested dependencies..."
+
+# Display command
+set -x
+
+# Try installing the debian requirements
+apt install $PACKAGES
