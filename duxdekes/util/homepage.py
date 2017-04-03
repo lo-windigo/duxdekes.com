06555032f4cd2d37f8435a5b1f23cad949588b19
Windigo <jacob@fragdev.com>
2017-04-02 21:36:16 -0700

    Create homepage data function to simplify and better organize code (also alter to use Oscar data models)

---
+++ duxdekes/util/homepage.py
@@ -0,0 +1,25 @@
+
+from oscar.core.loading import get_model
+
+def homepage_data(category_name):
+
+    Category = get_model('catalogue', 'Category')
+    Product = get_model('catalogue', 'Product')
+
+    parent_category = Category.objects.get(name=category_name)
+    categories = parent_category.get_descendants_and_self()
+
+    # Get the newest products from each category tree
+    products = []
+    for this_category in categories:
+        #products.append(this_category.product_set.browsable.order_by('-date_updated')[:4])
+        products.extend(Product.objects.filter(categories__in=[this_category,])[:4])
+
+    sorted(products,
+        key=lambda product: product.date_updated,
+        reverse=True)
+
+    return {
+        'products': products[:4],
+        'categories': categories,
+    }
