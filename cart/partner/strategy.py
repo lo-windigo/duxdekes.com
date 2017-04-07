0df964363fe2f5926c68f0e13c679929b9929df9
Windigo <jacob@fragdev.com>
2017-04-07 12:38:30 -0700

    Add a strategy to the partner app to allow for custom taxes (ie: only in NY)

---
+++ cart/partner/strategy.py
@@ -0,0 +1,50 @@
+from collections import namedtuple
+from decimal import Decimal as D
+
+from oscar.core.compat import user_is_authenticated
+from oscar.core.loading import get_class
+from oscar.apps.partner import strategy
+
+Unavailable = get_class('partner.availability', 'Unavailable')
+Available = get_class('partner.availability', 'Available')
+StockRequiredAvailability = get_class('partner.availability', 'StockRequired')
+UnavailablePrice = get_class('partner.prices', 'Unavailable')
+FixedPrice = get_class('partner.prices', 'FixedPrice')
+TaxInclusiveFixedPrice = get_class('partner.prices', 'TaxInclusiveFixedPrice')
+
+# A container for policies
+PurchaseInfo = namedtuple(
+    'PurchaseInfo', ['price', 'availability', 'stockrecord'])
+
+
+class Selector(object):
+    """
+    Responsible for returning the appropriate strategy class for a given
+    user/session.
+
+    This can be called in three ways:
+
+    #) Passing a request and user.  This is for determining
+       prices/availability for a normal user browsing the site.
+
+    #) Passing just the user.  This is for offline processes that don't
+       have a request instance but do know which user to determine prices for.
+
+    #) Passing nothing.  This is for offline processes that don't
+       correspond to a specific user.  Eg, determining a price to store in
+       a search index.
+
+    """
+
+    def strategy(self, request=None, user=None, **kwargs):
+        """
+        Return an instanticated strategy instance
+        """
+        # Default to the backwards-compatible strategy of picking the first
+        # stockrecord but charging zero tax.
+        return NYStrategy()
+
+
+class NYStrategy(strategy.UseFirstStockRecord, strategy.DeferredTax,
+            strategy.StockRequired, strategy.Structured)
+    pass
