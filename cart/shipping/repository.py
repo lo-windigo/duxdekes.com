2f2d1c5f8fedc14172f2c84e169969a2e5d0eb8d
Windigo <jacob@fragdev.com>
2017-04-07 12:53:40 -0700

    Changed the shipping "repository" to return different methods based on the address

--- cart/shipping/repository.py
+++ cart/shipping/repository.py
@@ -3,4 +3,12 @@ from . import methods
 
 
 class Repository(repository.Repository):
-        methods = (methods.UPS(),)
+
+    def get_available_shipping_methods(
+            self, basket, user=None, shipping_addr=None,
+            request=None, **kwargs):
+        
+        if shipping_addr and shipping_addr.country.code == 'US':
+            return (methods.DomesticShipping(),)
+        else:
+            return (methods.InternationalShipping(),)
