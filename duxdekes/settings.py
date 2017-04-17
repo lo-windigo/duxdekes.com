b569c716f7069782ea8a6a05c7be7ba336df01e5
Windigo <jacob@fragdev.com>
2017-04-16 21:52:43 -0700

    Remove lots of extraneous options from the Dashboard

--- duxdekes/settings.py
+++ duxdekes/settings.py
@@ -14,6 +14,7 @@ import os
 from oscar import get_core_apps, OSCAR_MAIN_TEMPLATE_DIR
 from oscar.defaults import *
 from django.urls import reverse_lazy
+from django.utils.translation import ugettext_lazy as _
 
 
 ####################
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 ] + get_core_apps( [
     'cart.catalogue',
     'cart.checkout',
+    'cart.dashboard',
     'cart.partner',
     'cart.shipping',
 ], )
@@ -133,6 +135,126 @@ OSCAR_HIDDEN_FEATURES = [
     'reviews',
     'wishlists',
 ]
+
+OSCAR_DASHBOARD_NAVIGATION = [
+#    {
+#        'label': _('Dashboard'),
+#        'icon': 'icon-th-list',
+#        'url_name': 'dashboard:index',
+#    },
+    {
+        'label': _('Catalogue'),
+        'icon': 'icon-sitemap',
+        'children': [
+            {
+                'label': _('Products'),
+                'url_name': 'dashboard:catalogue-product-list',
+            },
+#            {
+#                'label': _('Product Types'),
+#                'url_name': 'dashboard:catalogue-class-list',
+#            },
+            {
+                'label': _('Categories'),
+                'url_name': 'dashboard:catalogue-category-list',
+            },
+#            {
+#                'label': _('Ranges'),
+#                'url_name': 'dashboard:range-list',
+#            },
+#            {
+#                'label': _('Low stock alerts'),
+#                'url_name': 'dashboard:stock-alert-list',
+#            },
+        ]
+    },
+    {
+        'label': _('Fulfilment'),
+        'icon': 'icon-shopping-cart',
+        'children': [
+            {
+                'label': _('Orders'),
+                'url_name': 'dashboard:order-list',
+            },
+            {
+                'label': _('Statistics'),
+                'url_name': 'dashboard:order-stats',
+            },
+#            {
+#                'label': _('Partners'),
+#                'url_name': 'dashboard:partner-list',
+#            },
+            # The shipping method dashboard is disabled by default as it might
+            # be confusing. Weight-based shipping methods aren't hooked into
+            # the shipping repository by default (as it would make
+            # customising the repository slightly more difficult).
+            # {
+            #     'label': _('Shipping charges'),
+            #     'url_name': 'dashboard:shipping-method-list',
+            # },
+        ]
+    },
+#    {
+#        'label': _('Customers'),
+#        'icon': 'icon-group',
+#        'children': [
+#            {
+#                'label': _('Customers'),
+#                'url_name': 'dashboard:users-index',
+#            },
+#            {
+#                'label': _('Stock alert requests'),
+#                'url_name': 'dashboard:user-alert-list',
+#            },
+#        ]
+#    },
+#    {
+#        'label': _('Offers'),
+#        'icon': 'icon-bullhorn',
+#        'children': [
+#            {
+#                'label': _('Offers'),
+#                'url_name': 'dashboard:offer-list',
+#            },
+#            {
+#                'label': _('Vouchers'),
+#                'url_name': 'dashboard:voucher-list',
+#            },
+#        ],
+#    },
+    {
+        'label': _('Content'),
+        'icon': 'icon-folder-close',
+        'children': [
+            {
+                'label': _('Content blocks'),
+                'url_name': 'dashboard:promotion-list',
+            },
+            {
+                'label': _('Content blocks by page'),
+                'url_name': 'dashboard:promotion-list-by-page',
+            },
+            {
+                'label': _('Pages'),
+                'url_name': 'dashboard:page-list',
+            },
+            {
+                'label': _('Email templates'),
+                'url_name': 'dashboard:comms-list',
+            },
+#            {
+#                'label': _('Reviews'),
+#                'url_name': 'dashboard:reviews-list',
+#            },
+        ]
+    },
+#    {
+#        'label': _('Reports'),
+#        'icon': 'icon-bar-chart',
+#        'url_name': 'dashboard:reports-index',
+#    },
+]
+
 
 
 ##################
