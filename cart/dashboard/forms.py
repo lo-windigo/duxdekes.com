4a6631956fa46fe0fcaf0265cd2d8ba5d04908de
Windigo <jacob@fragdev.com>
2017-04-22 21:12:22 -0700

    Added all of the right fields for an unfinished blank

--- cart/dashboard/forms.py
+++ cart/dashboard/forms.py
@@ -5,9 +5,7 @@ class ProductForm(forms.Form):
     """
     A generic product form, containing common fields
     """
-    description = forms.CharField(label="Product Description",
-            max_length=200)
-    image = forms.ImageField(required=False)
+    title = forms.CharField(label="Title", max_length=200)
 
 
 class UnfinishedForm(ProductForm):
@@ -15,9 +13,27 @@ class UnfinishedForm(ProductForm):
     A form specifically tailored to creating an Unfinished Blank product
     """
     pine_id = forms.CharField(label="Pine ID",
-            max_length=64)
-    pine_price = forms.Field(label="Pine Price",
-            max_length=200)
+            max_length=64,
+            required=False)
+    pine_price = forms.DecimalField(label="Pine Price",
+            min_value=0,
+            decimal_places=2,
+            max_digits=12,
+            required=False)
+    tupelo_id = forms.CharField(label="Tupelo ID",
+            max_length=64,
+            required=False)
+    tupelo_price = forms.DecimalField(label="Tupelo Price",
+            min_value=0,
+            decimal_places=2,
+            max_digits=12,
+            required=False)
+    feet_price = forms.DecimalField(label="Optional Feet Price",
+            min_value=0,
+            decimal_places=2,
+            max_digits=12,
+            required=False)
+
 
 
 class FinishedForm(ProductForm):
