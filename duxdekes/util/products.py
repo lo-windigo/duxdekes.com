cda2de534345f67201c3d6c286d5c2730ec8c99c
Windigo <jacob@fragdev.com>
2017-05-17 14:11:48 -0700

    Drastically change product functions to auto-calculate material SKUs, allow updating of SKU/UPCs, implemented feet pricing, etc.

--- duxdekes/util/products.py
+++ duxdekes/util/products.py
@@ -14,10 +14,20 @@ Product = get_model('catalogue', 'Product')
 Partner = get_model('partner', 'Partner')
 StockRecord = get_model('partner', 'StockRecord')
 
-# Get the prerequisite related objects
-PARTNER=Partner.objects.get(name='Dux Dekes')
-UNFINISHED_CLASS=ProductClass.objects.get(name='Unfinished Blanks')
 
+
+def get_partner():
+    try:
+        return Partner.objects.get(name='Dux Dekes')
+    except Exception:
+        return None
+
+
+def get_unfinished_class():
+    try:
+        return ProductClass.objects.get(name='Unfinished Blanks')
+    except Exception:
+        return None
 
 
 def get_pine(unfinished_blank):
@@ -52,8 +62,9 @@ def save_unfinished(**kwargs):
 
     Supported Args:
     - title: Name/Description of the product
-    - pine_price, pine_sku: price and SKU of a pine decoy
-    - tupelo_price, tupelo_sku: price and SKU of a tupelo decoy
+    - upc: Stock ID of this product
+    - pine_price, pine_upc: price and SKU of a pine decoy
+    - tupelo_price, tupelo_upc: price and SKU of a tupelo decoy
     - feet_price: Price of optional feet
     - ...
     """
@@ -62,22 +73,29 @@ def save_unfinished(**kwargs):
 
     if updating:
         product = kwargs['instance']
+        old_upc = product.upc
     else:
         product = Product()
         product.structure = Product.PARENT
-        product.product_class = UNFINISHED_CLASS
+        product.product_class = get_unfinished_class()
 
     product.title = kwargs['title']
+    product.upc = kwargs['upc']
     product.save()
 
     # If there are pine or Tupelo values, add them as sub-products
-    if kwargs['pine_sku'] and kwargs['pine_price']:
-        save_pine(product, **kwargs)
+    if kwargs['pine_price']:
+        pine_args = kwargs
+        pine_args.update({'old_upc': old_upc,})
+        save_pine(product, **pine_args)
+
     elif updating:
         remove_pine(product)
 
-    if kwargs['tupelo_sku'] and kwargs['tupelo_price']:
-        save_tupelo(product, **kwargs)
+    if kwargs['tupelo_price']:
+        tupelo_args = kwargs
+        tupelo_args.update({'old_upc': old_upc,})
+        save_tupelo(product, **tupelo_args)
     elif updating:
         remove_tupelo(product)
 
@@ -90,8 +108,13 @@ def save_unfinished_material(product, **kwargs):
     out of
     """
     try:
-        variant = product.children.get(title__startswith=kwargs['name'])
-        stock = StockRecord.objects.get(product=variant)
+        if not kwargs['old_upc']:
+            raise ObjectDoesNotExist()
+
+        stock = StockRecord.objects.filter(
+                product__in=product.children.all()).get(
+                partner_sku=kwargs['old_upc'])
+        variant = stock.product
 
     except (ObjectDoesNotExist, MultipleObjectsReturned) as e:
         variant = Product()
@@ -100,47 +123,102 @@ def save_unfinished_material(product, **kwargs):
 
         stock = StockRecord()
         stock.product = variant
-        stock.partner = PARTNER
+        stock.partner = get_partner()
 
-    variant.title = '{} - {}'.format(kwargs['name'], product.title)
+    variant.title = '{} — {}'.format(kwargs['name'], product.title)
     variant.save()
 
-    stock.partner_sku = kwargs['sku'] 
+    stock.partner_sku = kwargs['upc'] 
     stock.price_excl_tax = kwargs['price']
     stock.save()
 
 
 def save_pine(product, **kwargs):
+    """
+    A wrapper around save_material that provides the sensible defaults for pine
+    """
+    upc_format = '{}_P'
+    upc = upc_format.format(kwargs['upc'])
+
+    if kwargs['old_upc']:
+        old_upc = upc_format.format(kwargs['old_upc'])
+    else:
+        old_upc = None
+
     save_unfinished_material(product,
             name=PINE,
-            sku=kwargs['pine_sku'],
+            upc=upc,
+            old_upc=old_upc,
             price=kwargs['pine_price'])
+
+    if kwargs['feet_price']:
+        save_pine_feet(product, **kwargs)
 
 
 def save_pine_feet(product, **kwargs):
+    """
+    A wrapper around save_material that provides the sensible defaults for pine
+    with optional feet
+    """
     name_with_feet = '{} {}'.format(PINE, FEET)
     price_with_feet = int(kwargs['pine_price']) + int(kwargs['feet_price'])
-    sku_with_feet = kwargs['pine_sku'] + '_F'
+    upc_format = '{}_PF'
+    upc = upc_format.format(kwargs['upc'])
+
+    if kwargs['old_upc']:
+        old_upc = upc_format.format(kwargs['old_upc'])
+    else:
+        old_upc = None
+
     save_unfinished_material(product,
             name=name_with_feet,
-            sku=sku_with_feet,
+            upc=upc,
+            old_upc=old_upc,
             price=price_with_feet)
 
 
 def save_tupelo(product, **kwargs):
+    """
+    A wrapper around save_material that provides the sensible defaults for
+    products offered in tupelo
+    """
+    upc_format = '{}_T'
+    upc = upc_format.format(kwargs['upc'])
+
+    if kwargs['old_upc']:
+        old_upc = upc_format.format(kwargs['old_upc'])
+    else:
+        old_upc = None
+
     save_unfinished_material(product,
             name=TUPELO,
-            sku=kwargs['tupelo_sku'],
+            upc=upc,
+            old_upc=old_upc,
             price=kwargs['tupelo_price'])
+
+    if kwargs['feet_price']:
+        save_tupelo_feet(product, **kwargs)
 
 
 def save_tupelo_feet(product, **kwargs):
+    """
+    A wrapper around save_material that provides the sensible defaults for
+    tupelo, with optional feet
+    """
     name_with_feet = '{} {}'.format(TUPELO, FEET)
     price_with_feet = int(kwargs['tupelo_price']) + int(kwargs['feet_price'])
-    sku_with_feet = kwargs['tupelo_sku'] + '_F'
+    upc_format = '{}_TF'
+    upc = upc_format.format(kwargs['upc'])
+
+    if kwargs['old_upc']:
+        old_upc = upc_format.format(kwargs['old_upc'])
+    else:
+        old_upc = None
+
     save_unfinished_material(product,
             name=name_with_feet,
-            sku=sku_with_feet,
+            upc=upc,
+            old_upc=old_upc,
             price=price_with_feet)
 
 
