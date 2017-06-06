39db32f91b06373a31a593e0e6d2141b91eaada3
Windigo <jacob@fragdev.com>
2017-06-06 11:06:20 -0700

    Add tentative models for the two types of instructions

--- cart/catalogue/models.py
+++ cart/catalogue/models.py
@@ -1,1 +1,21 @@
 from oscar.apps.catalogue.models import *  # noqa
+from django.contrib.flatpages import FlatPage
+from django.db import models
+
+
+
+class InstructionProduct(Product):
+    """
+    Override the usual product model to allow attaching an unfinished
+    blank to bundle
+    """
+    blank = models.ForeignKey(Product,
+            blank = True,
+            null = True)
+
+
+class InstructionProduct(Product):
+    extra_content = models.ForeignKey(FlatPage,
+            blank = True,
+            null = True)
+
