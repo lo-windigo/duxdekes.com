2275a9365755b81daff7f7ab1a7391f41688bbc6
Windigo <jacob@fragdev.com>
2017-06-13 09:21:59 -0700

    Fix the Instructions models - Bauer instructions are the same as the other Instruction sets

--- cart/catalogue/models.py
+++ cart/catalogue/models.py
@@ -3,25 +3,26 @@ from oscar.core.loading import get_model
 from django.db import models
 
 
-FlatPage = get_model('flatpages', 'FlatPage')
 
 
-class InstructionProduct(Product):
+
+class InstructionsProduct(Product):
     """
     Override the usual product model to allow attaching an unfinished
     blank to bundle
     """
     blank = models.ForeignKey(Product,
-            related_name = 'matching_blank',
-            blank = True,
-            null = True)
+            related_name='matching_blank',
+            required=False,
+            blank=True,
+            null=True)
 
 
-class BauerProduct(Product):
-    """
-    Override the usual product model to include extra descriptive content
-    """
-    extra_content = models.ForeignKey(FlatPage,
-            blank = True,
-            null = True)
+#class BookInstructionsProduct(Product):
+#    """
+#    Override the usual product model to include extra descriptive content
+#    """
+#    extra_content = models.ForeignKey(FlatPage,
+#            blank = True,
+#            null = True)
 
