74d60a956b5a9a7cc49bf49608afabed809f7e31
Windigo <jacob@fragdev.com>
2017-06-21 14:54:44 -0700

    Add tests for some instructions functions

--- duxdekes/tests/tests_products.py
+++ duxdekes/tests/tests_products.py
@@ -9,7 +9,8 @@ class ProductUtilities(TestCase):
     """
     def setUp(self):
         self.partner_class = get_model('partner', 'Partner')
-        self.product_class = get_model('catalogue', 'ProductClass')
+        self.product_class = get_model('catalogue', 'Product')
+        self.product_class_class = get_model('catalogue', 'ProductClass')
 
 
     def test_partner(self):
@@ -28,24 +29,27 @@ class ProductUtilities(TestCase):
 
 
     def test_product_classes(self):
-        finished_name = 'Finished Decoys'
-        finished_class = products.get_finished_class()
-        unfinished_name = 'Unfinished Blanks'
-        unfinished_class = products.get_unfinished_class()
+        product_classes = (
+                    ('Finished Decoys', products.get_finished_class()),
+                    ('Instructions', products.get_instructions_class()),
+                    ('Unfinished Blanks', products.get_unfinished_class()),
+                )
 
-        self.assertIsInstance(finished_class, self.product_class, msg='''
-        get_finished_class() returned an instance of ProductClass
-        ''')
-        self.assertIsInstance(unfinished_class, self.product_class, msg='''
-        get_unfinished_class() returned an instance of ProductClass
-        ''')
+        for name, product_class in product_classes:
 
-        self.assertEqual(finished_class.name, finished_name, msg='''
-        Finished ProductClass name matches the expected class name
-        ''')
-        self.assertEqual(unfinished_class.name, unfinished_name, msg='''
-        Unfinished ProductClass name matches the expected class name
-        ''')
+            # ProductClass is returned
+            self.assertIsInstance(product_class,
+                self.product_class_class,
+                msg='''
+                    {} getter method returned an instance of ProductClass
+                '''.format(name))
+
+            # Class name matches expected name
+            self.assertEqual(product_class.name,
+                name,
+                msg='''
+                    {} ProductClass name matches the expected name
+                    '''.format(name))
 
 
     def test_create_pine_product(self):
@@ -55,33 +59,31 @@ class ProductUtilities(TestCase):
         pass
 
 
-    def test_(self):
+    def test_create_instructions_no_blank(self):
+        """
+        Set up a new instructions product, without a matching blank
+        """
+        data = {
+                'price': 235.24,
+                'title': 'How to paint a goose blank',
+                'upc': 'TESTGOOSE1',
+        }
+
+        instructions = products.save_instructions(**data)
+
+        self.assertIsInstance(instructions, self.product_class, msg="""
+        Created instruction object is a Product
+        """)
+        self.assertTrue(instructions.is_parent, msg="""
+        Instruction object should be a parent product
+        """)
+        # This test fails...?
+        self.assertEqual(instructions.children.count(), 1, msg="""
+        Instruction object created without associated blank should have one
+        child product
+        """)
+
+
+    def test_create_instructions_with_blank(self):
         pass
 
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
-
-
-    def test_(self):
-        pass
