07aa2b64c6bb221e0f5955ffeb5168e7e87c7a07
Windigo <jacob@fragdev.com>
2017-07-11 10:00:02 -0700

    Add the generic request data to the base class

--- ups_json/base.py
+++ ups_json/base.py
@@ -1,16 +1,58 @@
 
 import json
+from urllib import request
 
 
 class UPSBase():
     """
     A base for requests to the UPS JSON API
     """
-    def __init__(self, account, key, debug=False):
+    debug = False
+    request_data = {}
+
+    def __init__(self, account, password, license_number=None, testing=False):
         """
-        Save default credentials, values
+        Save user credentials, values to the request data
         """
-        self.account = account
-        self.api_key = key
-        self.debug = debug
+        self.request_data['UPSSecurity'] = {
+                'Username': account,
+                'Password': password,
+                }
+
+        self.request_data['ServiceAccessToken'] = {
+                'AccessLicenseNumber': license_number,
+                }
+
+        self.testing = testing
+
+    def request(self):
+        """
+        Take in the required information, and encode it in the JSON format that
+        is required by the UPS API
+        """
+        json_data = json.dumps(self.request_data)
+        url = self.get_url()
+
+        api_request = request.Request(url,
+                headers = {
+                'Access-Control-Allow-Headers':
+                    'Origin, X-Requested-With, Content-Type, Accept',
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Origin': '*',
+                'Content-type': 'application/json',
+                },
+                data = json_data,
+                method = 'POST')
+        api_response = request.urlopen(api_request)
+
+        return json.loads(api_response.read())
+
+    def get_url(self):
+        """
+        Get the URL to send the request to
+        """
+        if self.testing:
+            return self.TESTING_URL
+
+        return self.PRODUCTION_URL
 
