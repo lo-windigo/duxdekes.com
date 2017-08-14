53cea84f13cf879309d1fe96c81a13a33de19176
Windigo <jacob@fragdev.com>
2017-08-14 10:46:54 -0700

    Update product utility compatibility functions with proper namespace

--- duxdekes/util/products.py
+++ duxdekes/util/products.py
@@ -1,4 +1,4 @@
-from cart.catalogue import util
+from cart.catalogue.util import products
 
 
 """
@@ -6,71 +6,71 @@ This has moved - include redirect functions to avoid having to go back and
 change everything!
 """
 def get_partner():
-    return util.get_partner()
+    return products.get_partner()
 
 def get_finished_class():
-    return util.get_finished_class()
+    return products.get_finished_class()
 
 def get_instructions_class():
-    return util.get_instructions_class()
+    return products.get_instructions_class()
 
 def get_book_class():
-    return util.get_book_class()
+    return products.get_book_class()
 
 def get_unfinished_class():
-    return util.get_unfinished_class()
+    return products.get_unfinished_class()
 
 def get_material(unfinished_blank, upc_format, original_upc = None):
-    return util.get_material(unfinished_blank, upc_format, original_upc = None)
+    return products.get_material(unfinished_blank, upc_format, original_upc = None)
 
 def get_instructions_alone(instructions, original_upc = None):
-    return util.get_instructions_alone(instructions, original_upc = None)
+    return products.get_instructions_alone(instructions, original_upc = None)
 
 def get_instructions_with_blank(instructions, original_upc = None):
-    return util.get_instructions_with_blank(instructions, original_upc = None)
+    return products.get_instructions_with_blank(instructions, original_upc = None)
 
 def get_pine(unfinished_blank, original_upc = None):
-    return util.get_pine(unfinished_blank, original_upc = None)
+    return products.get_pine(unfinished_blank, original_upc = None)
 
 def get_pine_feet(unfinished_blank, original_upc = None):
-    return util.get_pine_feet(unfinished_blank, original_upc = None)
+    return products.get_pine_feet(unfinished_blank, original_upc = None)
 
 def get_tupelo(unfinished_blank, original_upc = None):
-    return util.get_tupelo(unfinished_blank, original_upc = None)
+    return products.get_tupelo(unfinished_blank, original_upc = None)
 
 def get_tupelo_feet(unfinished_blank, original_upc = None):
-    return util.get_tupelo_feet(unfinished_blank, original_upc = None)
+    return products.get_tupelo_feet(unfinished_blank, original_upc = None)
 
 def save_finished(**kwargs):
-    return util.save_finished(**kwargs)
+    return products.save_finished(**kwargs)
 
 def save_unfinished(**kwargs):
-    return util.save_unfinished(**kwargs)
+    return products.save_unfinished(**kwargs)
 
 def save_unfinished_material(product, **kwargs):
-    util.save_unfinished_material(product, **kwargs)
+    products.save_unfinished_material(product, **kwargs)
 
 def save_pine(product, **kwargs):
-    util.save_pine(product, **kwargs)
+    products.save_pine(product, **kwargs)
 
 def save_pine_feet(product, **kwargs):
-    util.save_pine_feet(product, **kwargs)
+    products.save_pine_feet(product, **kwargs)
 
 def save_tupelo(product, **kwargs):
-    util.save_tupelo(product, **kwargs)
+    products.save_tupelo(product, **kwargs)
 
 def save_tupelo_feet(product, **kwargs):
-    util.save_tupelo_feet(product, **kwargs)
+    products.save_tupelo_feet(product, **kwargs)
 
 def remove_pine(product, **kwargs):
-    util.remove_pine(product, **kwargs)
+    products.remove_pine(product, **kwargs)
 
 def remove_pine_feet(product, **kwargs):
-    util.remove_pine_feet(product, **kwargs)
+    products.remove_pine_feet(product, **kwargs)
 
 def remove_tupelo(product, **kwargs):
-    util.remove_tupelo(product, **kwargs)
+    products.remove_tupelo(product, **kwargs)
 
 def remove_tupelo_feet(product, **kwargs):
-    util.remove_tupelo_feet(product, **kwargs)
+    products.remove_tupelo_feet(product, **kwargs)
 
