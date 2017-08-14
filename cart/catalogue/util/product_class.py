90f428d9be3f678c9c77a68008b7b1981024b680
Windigo <jacob@fragdev.com>
2017-08-14 10:47:49 -0700

    Move the product class utilities to the catalogue app

---
+++ cart/catalogue/util/product_class.py
@@ -0,0 +1,21 @@
+from oscar.core.loading import get_model
+
+ProductAttribute = get_model('catalogue', 'ProductAttribute')
+ProductClass = get_model('catalogue', 'ProductClass')
+
+
+def make_class_attributes():
+    for product_class in ProductClass.objects.all():
+        ProductAttribute.objects.get_or_create(code='weight',
+                product_class=product_class,
+                defaults={
+                    'name': 'Weight',
+                    'type': ProductAttribute.FLOAT,
+                    })
+        ProductAttribute.objects.get_or_create(code='box',
+                product_class=product_class,
+                defaults={
+                    'name': 'Box used for shipping',
+                    'type': ProductAttribute.ENTITY,
+                    })
+
