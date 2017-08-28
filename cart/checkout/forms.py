abcbbeaeb53c9e673a27e781f9b22e03acbf83d2
Windigo <jacob@fragdev.com>
2017-08-28 11:39:52 -0700

    Fix Square form template placement, started adding forms/view classes for the payment portion of checkout

---
+++ cart/checkout/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class SquareNonceForm(forms.Form):
+    """
+    A form used to submit the Squareconnect nonce value
+    """
+    nonce = forms.CharField(max_length=300)
---
+++ cart/checkout/views.py
@@ -0,0 +1,6 @@
+from oscar.core.loading import get_class
+from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsView
+
+
+class PaymentDetailsView(OscarPaymentDetailsView):
+    pass
