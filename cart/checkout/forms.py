ed81a01ffcbdb5fe704fe97dd7ba8ec1a8c6457b
Windigo <jacob@fragdev.com>
2017-08-29 12:22:37 -0700

    Added handle_payment_details function

--- cart/checkout/forms.py
+++ cart/checkout/forms.py
@@ -6,3 +6,4 @@ class SquareNonceForm(forms.Form):
     A form used to submit the Squareconnect nonce value
     """
     nonce = forms.CharField(max_length=300)
+
--- cart/checkout/views.py
+++ cart/checkout/views.py
@@ -1,6 +1,18 @@
 from oscar.core.loading import get_class
 from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsView
+from . import forms
 
 
 class PaymentDetailsView(OscarPaymentDetailsView):
-    pass
+
+    def handle_payment_details_submission(self, request):
+        """
+        Handle Square payment form submission
+        """
+        square_form = forms.SquareNonceForm(request.POST)
+
+        if square_form.is_valid():
+            return self.render_preview(request, form=square_form)
+        
+        return self.render_payment_details(request, form=square_form)
+
