cbd29dcccfd83c30fe9a7331e42e7c0e25ef41a0
Windigo <jacob@fragdev.com>
2017-09-06 16:49:50 -0700

    Fix some issues with Square IDs and the nonce form

--- cart/checkout/forms.py
+++ cart/checkout/forms.py
@@ -5,5 +5,5 @@ class SquareNonceForm(forms.Form):
     """
     A form used to submit the Squareconnect nonce value
     """
-    nonce = forms.CharField(max_length=300)
+    nonce = forms.CharField(max_length=300, widget=forms.HiddenInput)
 
--- cart/checkout/views.py
+++ cart/checkout/views.py
@@ -1,9 +1,22 @@
+from django.conf import settings
 from oscar.core.loading import get_class
 from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsView
 from . import forms
 
 
 class PaymentDetailsView(OscarPaymentDetailsView):
+
+    def get_context_data(self, **kwargs):
+        """
+        Provide the square nonce form in context
+        """
+        ctx = super().get_context_data(**kwargs)
+
+        ctx['form'] = forms.SquareNonceForm()
+        ctx['square_app'] = settings.SQUARE_APPLICATION_ID
+
+        return ctx
+
 
     def handle_payment_details_submission(self, request):
         """
