bee0d0be01ef386612bbc95916baef0a7d71e027
Windigo <jacob@fragdev.com>
2018-02-03 10:57:16 -0800

    Simplify get_available_shipping_methods function

--- cart/shipping/repository.py
+++ cart/shipping/repository.py
@@ -5,18 +5,20 @@ from . import methods
 
 class Repository(repository.Repository):
 
-    def get_available_shipping_methods(
-            self, basket, user=None, shipping_addr=None,
-            request=None, **kwargs):
+    def get_available_shipping_methods(self, basket, user=None,
+            shipping_addr=None, request=None, **kwargs):
+
+        # We only have one shipping method so far
+        return (methods.DomesticShipping(shipping_addr),)
         
-        if shipping_addr:
+        #if shipping_addr:
             #if shipping_addr.country.code == 'US':
             #    return (methods.DomesticShipping(shipping_addr),)
             #else:
             #    return (methods.InternationalShipping(shipping_addr),)
-            return (methods.DomesticShipping(shipping_addr),)
-        else:
+            #return (methods.DomesticShipping(shipping_addr),)
+        #else:
             # We need a return value before details have been entered, for
             # presenting the basket originally
-            return (methods.DomesticShipping(),)
+            #return (methods.DomesticShipping(),)
 
