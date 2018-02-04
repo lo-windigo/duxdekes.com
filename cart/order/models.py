e531fc888f0dd9e38c6d2f2b663f54424c65a403
Windigo <jacob@fragdev.com>
2018-02-03 22:07:53 -0800

    Added "Final amount" field to order model

--- cart/order/models.py
+++ cart/order/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+from oscar.apps.order.abstract_models import AbstractOrder
+
+
+class Order(AbstractOrder):
+    """
+    Override the order model to add additional fields for post-order shipping
+    """
+    final_shipping = models.DecimalField("Final shipping charge",
+            decimal_places=2, max_digits=12, default=0)
+
+
 from oscar.apps.order.models import *  # noqa isort:skip
