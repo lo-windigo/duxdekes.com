e5848a5fb059e2ae48037385e24e6e73420d4c3a
Windigo <jacob@fragdev.com>
2018-03-31 21:45:50 +0000

    Fix EventHandler class to use PaymentEventType as an argument, improved error/exception handling, etc.

--- cart/order/processing.py
+++ cart/order/processing.py
@@ -1,10 +1,7 @@
 from duxdekes.util.square import adjust_charge, capture_payment
 from oscar.apps.order import exceptions
-from oscar.core.loading import get_model, get_class
 from oscar.apps.order.exceptions import InvalidPaymentEvent
 from oscar.apps.order.processing import EventHandler as OscarEventHandler
-
-PaymentEventType = get_model('order', 'PaymentEventType')
 
 
 class EventHandler(OscarEventHandler):
@@ -18,35 +15,41 @@ class EventHandler(OscarEventHandler):
         Integrate some Square-specific payment details
         """
 
-        # Finalize a previously authorized order
-        if event_type == 'capture':
-            try:
-                if order.total_incl_tax < amount:
-                    error = """
-                    The original authorized cost is less than the cost with shipping
-                    """
-                    raise ChargeCaptureException(error)
+        # Validate that we have valid amounts
+        if order.total_incl_tax < amount:
+            msg = 'The authorized cost cannot be less than the final cost'
+            raise InvalidPaymentEvent(msg)
 
+        # Finalize a previously authorized order
+        if event_type.code == 'capture':
+            try:
                 # Get the old transaction ID
-                auth_payment_event = order.payment_events.get(event_type__code='auth')
+                auth_payment_event = order.payment_events.filter(
+                        event_type__code='auth').latest('date_created')
                 auth_payment_reference = auth_payment_event.reference
-                kwargs['reference'] = capture_payment(auth_payment_reference)
+                capture_payment(auth_payment_reference)
+                kwargs['reference'] = auth_payment_reference
             except Exception as e:
                 msg = 'Could not get the authorization payment event'
                 raise InvalidPaymentEvent(msg) from e
 
         # Adjust the cost of an order
-        elif event_type == 'adjust': 
+        elif event_type.code == 'adjust': 
             try:
                 # Get the old transaction ID
-                cap_payment_event = order.payment_events.get(event_type__code='capture')
+                cap_payment_event = order.payment_events.filter(
+                        event_type__code='capture').latest('date_created')
                 cap_payment_reference = cap_payment_event.reference
-                kwargs['reference'] = adjust_charge(order.id,
-                        cap_payment_reference, order.total_incl_tax, amount)
             except Exception as e:
                 msg = 'Could not get the capture payment event'
                 raise InvalidPaymentEvent(msg) from e
 
+            try:
+                kwargs['reference'] = adjust_charge(order.number,
+                        cap_payment_reference, order.total_incl_tax, amount)
+            except Exception as e:
+                msg = 'Unable to adjust the charge'
+                raise InvalidPaymentEvent(msg) from e
 
         return super().handle_payment_event(order, event_type, amount, lines,
                 line_quantities, **kwargs)
