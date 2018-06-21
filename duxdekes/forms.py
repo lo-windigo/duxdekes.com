568a574658a7c8795f3952300f13a98c2cd6d944
Windigo <jacob@fragdev.com>
2018-06-21 08:49:39 -0700

    Fix the classes on the contact form inputs

--- duxdekes/forms.py
+++ duxdekes/forms.py
@@ -6,17 +6,17 @@ class ContactForm(forms.Form):
     """
     The form used to send messages through the Contact page
     """
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
+    name = forms.CharField(max_length=100,
+            widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     url = forms.CharField(required=False, label='Please ignore this field')
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
 
     def is_valid(self):
         """
         Check to make sure our false URL field is empty
         """
-
         if len(self.data['url']) > 0:
             return False
 
