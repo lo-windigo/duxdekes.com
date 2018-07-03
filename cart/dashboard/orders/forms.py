538dabf8a05dae068ce386f13687bb5135be69b2
Windigo <jacob@fragdev.com>
2018-07-03 13:05:49 -0700

    Added form for applying order discounts via the order detail page of the dashboard

--- cart/dashboard/orders/forms.py
+++ cart/dashboard/orders/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from oscar.core.loading import get_class, get_model
 
 
+class DiscountOrderForm(forms.Form):
+    """
+    A form used to accept a new basket total
+    """
+    final_basket_total = forms.DecimalField(label="Final basket total")
+
 class FinalizeOrderForm(forms.Form):
     """
     A form specifically tailored to creating Instructions product
