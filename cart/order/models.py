411dc59f690ea91ee63f98feb6bca91d92776f2b
Windigo <jacob@fragdev.com>
2018-07-06 13:06:33 -0700

    Add new property methods that will support customer order status notices

--- cart/order/models.py
+++ cart/order/models.py
@@ -12,18 +12,41 @@ class Order(AbstractOrder):
             decimal_places=2, max_digits=12, default=0)
 
     @property
-    def final_basket_charge(self):
+    def basket_charge_override(self):
+        """
+        Returns boolean indicating whether or not the basket total value has
+        been overridden
+        """
+        return self.final_basket_total and self.final_basket_total > 0
 
-        # If we have an override set on this order, return that value
+    @property
+    def shipping_charge_finalized(self):
+        """
+        Returns boolean indicating whether or not the shipping charge has
+        been finalized
+        """
+        return self.final_shipping and self.final_shipping > 0
+
+    @property
+    def final_basket_charge(self):
+        """
+        Return the final basket total, whether it is an adjusted value or the
+        default oscar value
+        """
         if self.basket_charge_override:
             return self.final_basket_total
 
-        # Default to the normal basket total
         return self.basket_total_incl_tax
 
     @property
-    def basket_charge_override(self):
-        return self.final_basket_total and self.final_basket_total > 0
+    def final_shipping_charge(self):
+
+        # If we have an override set on this order, return that value
+        if self.shipping_charge_finalized:
+            return self.final_shipping
+
+        # Default to the normal shipping total
+        return self.shipping_incl_tax
 
 
 from oscar.apps.order.models import *  # noqa isort:skip
