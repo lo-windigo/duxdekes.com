e3b0bad6e7595213f6af2f89dcb9ed94d2b5bc45
Windigo <jacob@fragdev.com>
2018-07-26 11:58:56 -0700

    Set the phone number field on the shipping address form to be required

--- cart/checkout/forms.py
+++ cart/checkout/forms.py
@@ -1,4 +1,18 @@
 from django import forms
+from oscar.apps.checkout.forms import \
+        ShippingAddressForm as OscarShippingAddressForm
+
+
+class ShippingAddressForm(OscarShippingAddressForm):
+    """
+    Override the shipping address form to make the phone number field required
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['phone_number'].required = True
+
+    class Meta(OscarShippingAddressForm.Meta):
+        pass
 
 
 class SquareNonceForm(forms.Form):
