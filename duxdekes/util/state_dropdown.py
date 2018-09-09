be0e24a135cafef880efda61510905230bb4b3d6
Windigo <jacob@fragdev.net>
2018-09-09 13:36:46 -0700

    Create a dict of state data from the raw statestyle data

--- duxdekes/util/state_dropdown.py
+++ duxdekes/util/state_dropdown.py
@@ -1,16 +1,22 @@
-import csv
+from statestyle.data import CROSSWALK
 
-def generate_state_dict(data_file):
+def generate_state_dict():
     """
     Generate a dict with all US states
-    *Uses latimes-statestyle data CSV*
+    *Uses latimes-statestyle data*
     """
-
-    data = list(csv.DictReader(open(data_file, "r")))
     states = {}
 
-    for state in data:
-        data.update({state['postal']: state['name']})
+    for postal, state in CROSSWALK.items():
+
+        # Ignore the integer, lowercase, and any other indices aside from
+        # uppercase, two-letter state abbreviations.
+        #
+        # If this state/territory's postal abbreviation has not been added yet,
+        # insert it into the dict
+        if isinstance(postal, str) and len(postal) == 2 and \
+                postal.isupper() and postal not in states:
+            states.update({postal: state['name']})
 
     return states
 
