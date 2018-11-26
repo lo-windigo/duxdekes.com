21b55106954dc274a73c7204a2de1981cf27b726
Windigo <jacob@fragdev.net>
2018-11-25 19:09:11 -0800

    Attempt to move some data initialization around to see if that gets rating working with the new free shipping code

--- cart/shipping/methods.py
+++ cart/shipping/methods.py
@@ -30,9 +30,37 @@ class DomesticShipping(methods.Base):
 
     def __init__(self, shipping_addr=None):
         """
-        Quickly init the default box, and save the shipping address
+        Save the shipping address for use later, and set up the UPS rating
+        objects and address
         """
-        if not shipping_addr:
+        if shipping_addr:
+            # Compile the address dictionary
+            self.ups_address = {
+                    'name': '{} {}'.format(getattr(self.shipping_addr,
+                        'first_name', ''),
+                        getattr(self.shipping_addr, 'last_name', '')),
+                    'postal_code': self.shipping_addr.postcode,
+                    'city': self.shipping_addr.line4,
+                    'state_province': self.shipping_addr.state,
+                    'country_code': self.shipping_addr.country.code,
+                    'lines': [],
+                }
+
+            # Append any address lines to the list
+            for i in range(1, 4):
+                try:
+                    address['lines'].append(getattr(self.shipping_addr,
+                        'line{}'.format(i)))
+                except:
+                    pass
+
+            # Initialize the UPS rating object
+            ups_settings = models.UPSSettings.get_settings()
+            self.ups_rating = UPSRating(ups_settings.user,
+                    ups_settings.password,
+                    ups_settings.license,
+                    ups_settings.testing)
+        else:
             self.name = '{} (rated after address entered)'.format(self.name)
 
 
@@ -43,40 +71,14 @@ class DomesticShipping(methods.Base):
         lines = basket.all_lines()
 
         # If there is no address, or no lines in the basket
-        if not self.shipping_addr or not lines:
+        if not (self.shipping_addr and lines):
             return prices.Price(
                 currency=basket.currency,
                 excl_tax=D(0),
                 incl_tax=D(0))
 
-        # Initialize a rate request object
-        ups_settings = models.UPSSettings.get_settings()
-        ups = UPSRating(ups_settings.user,
-                ups_settings.password,
-                ups_settings.license,
-                ups_settings.testing)
- 
-        # Compile the address dictionary
-        address = {
-                'name': '{} {}'.format(getattr(self.shipping_addr,
-                    'first_name', ''),
-                    getattr(self.shipping_addr, 'last_name', '')),
-                'postal_code': self.shipping_addr.postcode,
-                'city': self.shipping_addr.line4,
-                'state_province': self.shipping_addr.state,
-                'country_code': self.shipping_addr.country.code,
-                'lines': [],
-                }
 
-        # Append any address lines to the list
-        for i in range(1, 4):
-            try:
-                address['lines'].append(getattr(self.shipping_addr,
-                    'line{}'.format(i)))
-            except:
-                pass
-
-        # Rate all items in the basket. Add $1 to pay for shipping container
+        # Add $1 to this basket's rate to cover shipping container
         rate = D(1)
         
         for item in lines:
@@ -84,7 +86,7 @@ class DomesticShipping(methods.Base):
 
         # Calculate taxes
         rate_incl_tax = rate
-        if self.shipping_addr and self.shipping_addr.state.upper() == 'NY':
+        if self.shipping_addr.state.upper() == 'NY':
             rate_incl_tax += tax.calculate_sales_tax(rate) 
 
         return prices.Price(
@@ -93,34 +95,34 @@ class DomesticShipping(methods.Base):
             incl_tax=rate_incl_tax)
 
 
-def rate_item(product, ups, address):
-    """
-    Rate a single item for shipping
-    """
+    def rate_item(self, product):
+        """
+        Rate a single item for shipping
+        """
 
-    # Make sure we are pulling the correct attributes for child products
-    if product.structure == Product.CHILD:
-        product = product.parent
+        # Make sure we are pulling the correct attributes for child products
+        if product.structure == Product.CHILD:
+            product = product.parent
 
-    # Before rating, make sure this item doesn't have free shipping
-    try:
-        if product.attr.free_shipping:
-            return D(0)
-    except:
-        pass
+        # Before rating, make sure this item doesn't have free shipping
+        try:
+            if product.attr.free_shipping:
+                return D(0)
+        except AttributeError:
+            pass
 
-    # Try to get the package rate from UPS
-    try:
-        box = product.attr.box 
-        item_rate = ups.get_rate(box.length, box.width, box.height,
-                product.attr.weight, address, settings.UPS_SHIPPER)
+        # Try to get the package rate from UPS
+        try:
+            box = product.attr.box 
+            item_rate = self.ups_rating.get_rate(box.length, box.width, box.height,
+                    product.attr.weight, self.ups_address, settings.UPS_SHIPPER)
 
-    except Exception as e:
-        err = 'Could not rate "%s" for shipping; adding $20. Error: %s'
-        logger.warn(err, product.upc, e)
+        except Exception as e:
+            err = 'Could not rate "%s" for shipping; adding $20. Error: %s'
+            logger.warn(err, product.upc, e)
 
-        # If the rate cannot be gathered, add in a very high estimate
-        item_rate = D(20)
+            # If the rate cannot be gathered, add in a very high estimate
+            item_rate = D(20)
 
-    return item_rate
+        return item_rate
 
